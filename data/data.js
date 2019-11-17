d1b73b1158da31b62aafeb5589502d5940429d38
Asier Marqués <asiermarques@packlink.com>
2019-11-17 23:58:57 +0100

    fix texto

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ module.exports =
     "logo": "/logo.png",
     "conference_name": "Bilbostack",
     "conference_claim": "9ª Edición de la cita tecnológica anual más esperada en&nbsp;Bilbao",
-    "conference_date": "25 de Enero",
+    "conference_date": "25 de Enero, Bilbao",
     "conference_hashtag": "#bilbostack20",
     "location": {
         "gmaps_iframe_url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.255497835498!2d-2.947592084396059!3d43.26702288533392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDPCsDE2JzAxLjMiTiAywrA1Nic0My41Ilc!5e0!3m2!1ses!2ses!4v1543099024128",
