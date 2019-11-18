45f3cf089788ecb327841ba0429ebeb16886b025
Asier Marqués <asiermarques@packlink.com>
2019-11-18 18:22:46 +0100

    fix sponsors fucking blocker 2

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -11,13 +11,17 @@ export default ({ sponsorBlocks }) => {
                         <h2>Patrocinadores</h2>
 
                         {sponsorBlocks.map( (block, index_block) => (
-                        <div key={index_block} className="list_block">
+                        <div key={index_block} className="list">
                             {block.name && <h3>{block.name}</h3>}
                             {block.sponsors.map( (sponsor, index) => <a key={index}
                                                                         href={sponsor.link} 
                                                                         title={sponsor.name}
                                                                         class="item">
-                                    <img src={sponsor.image} alt={sponsor.name}/>
+                                    <img src={sponsor.image} alt={sponsor.name} 
+                                         style={{
+                                            height: block.height_em + 'em',
+                                            width: 'auto'
+                                            }}/>
                             </a> )}
                         </div>
                         ))}
