6cbf6e19f278d2a5f46d469af77e75ca6cd9ed60
Fran Mosteiro <fmosteiro@theinit.com>
2019-11-26 09:39:30 +0100

    change cta, 3th wave

--- data/data.js
+++ data/data.js
@@ -17,7 +17,7 @@ module.exports =
         ]
     },
     "header_banner": {
-        "cta_pre_text": "2º oleada de 'Entradas Generales' a la venta el 24/11/2019 20:00h",
+        "cta_pre_text": "3ª oleada de 'Entradas Generales' a la venta el 26/11/2019 14:00h",
         "cta_text": "¡Entradas disponibles!",
         "cta_url": "https://bilbostack2020.eventbrite.es"
     },
